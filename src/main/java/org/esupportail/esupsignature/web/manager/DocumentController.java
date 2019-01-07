8f90d697b62b07217ca6ee4c3d5f6ea1c6565f9f
David Lemaignent <david.lemaignent@univ-rouen.fr>
2019-01-07 16:39:47 +0100

    ajout controleurs roo

---
+++ src/main/java/org/esupportail/esupsignature/web/manager/DocumentController.java
@@ -0,0 +1,11 @@
+package org.esupportail.esupsignature.web.manager;
+import org.esupportail.esupsignature.domain.Document;
+import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/manager/documents")
+@Controller
+@RooWebScaffold(path = "documents", formBackingObject = Document.class)
+public class DocumentController {
+}
---
+++ src/main/java/org/esupportail/esupsignature/web/manager/UserController.java
@@ -0,0 +1,11 @@
+package org.esupportail.esupsignature.web.manager;
+import org.esupportail.esupsignature.domain.User;
+import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/manager/users")
+@Controller
+@RooWebScaffold(path = "users", formBackingObject = User.class)
+public class UserController {
+}
