e46d3eaec4d4c49411d4bc85a93a8dde288465a7
David Lemaignent <david.lemaignent@univ-rouen.fr>
2019-03-01 14:41:21 +0100

    fix load keystore

--- src/main/java/org/esupportail/esupsignature/service/FileService.java
+++ src/main/java/org/esupportail/esupsignature/service/FileService.java
@@ -116,4 +116,21 @@ public class FileService {
 		}
 	}
 	
+	public void copyFile(File source, File dest) throws IOException {
+	    InputStream is = null;
+	    OutputStream os = null;
+	    try {
+	        is = new FileInputStream(source);
+	        os = new FileOutputStream(dest);
+	        byte[] buffer = new byte[1024];
+	        int length;
+	        while ((length = is.read(buffer)) > 0) {
+	            os.write(buffer, 0, length);
+	        }
+	    } finally {
+	        is.close();
+	        os.close();
+	    }
+	}
+	
 }
