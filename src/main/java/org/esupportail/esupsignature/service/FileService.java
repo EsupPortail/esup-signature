c8a3ecd76b97747a5865875f1243e4302f8b5797
David Lemaignent <david.lemaignent@univ-rouen.fr>
2019-04-26 16:48:21 +0200

    correction comment require

--- src/main/java/org/esupportail/esupsignature/service/FileService.java
+++ src/main/java/org/esupportail/esupsignature/service/FileService.java
@@ -3,9 +3,15 @@ package org.esupportail.esupsignature.service;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.font.FontRenderContext;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
+import java.awt.image.FilteredImageSource;
+import java.awt.image.ImageFilter;
+import java.awt.image.ImageProducer;
+import java.awt.image.RGBImageFilter;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -169,4 +175,40 @@ public class FileService {
 	    }
 	}
 	
+	//TODO gestion de la transparence
+	public Image makeColorTransparent(final BufferedImage im, final Color color)
+	   {
+	      final ImageFilter filter = new RGBImageFilter()
+	      {
+	         // the color we are looking for (white)... Alpha bits are set to opaque
+	         public int markerRGB = color.getRGB() | 0xFFFFFFFF;
+
+	         public final int filterRGB(final int x, final int y, final int rgb)
+	         {
+	            if ((rgb | 0xFF000000) == markerRGB)
+	            {
+	               // Mark the alpha bits as zero - transparent
+	               return 0x00FFFFFF & rgb;
+	            }
+	            else
+	            {
+	               // nothing to do
+	               return rgb;
+	            }
+	         }
+	      };
+
+	      final ImageProducer ip = new FilteredImageSource(im.getSource(), filter);
+	      return Toolkit.getDefaultToolkit().createImage(ip);
+	   }
+	
+	public BufferedImage imageToBufferedImage(final Image image)
+	   {
+	      final BufferedImage bufferedImage =
+	         new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+	      final Graphics2D g2 = bufferedImage.createGraphics();
+	      g2.drawImage(image, 0, 0, null);
+	      g2.dispose();
+	      return bufferedImage;
+	    }
 }
