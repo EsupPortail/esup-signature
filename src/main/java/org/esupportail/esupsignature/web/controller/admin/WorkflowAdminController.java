6173f500865e8b837b45c98e395147c86b7f8858
David Lemaignent <david.lemaignent@univ-rouen.fr>
2020-09-10 17:03:52 +0200

    fix wf filter

--- src/main/java/org/esupportail/esupsignature/web/controller/admin/WorkflowAdminController.java
+++ src/main/java/org/esupportail/esupsignature/web/controller/admin/WorkflowAdminController.java
@@ -88,7 +88,8 @@ public class WorkflowAdminController {
 	public String list(@RequestParam(name = "displayWorkflowType", required = false) String displayWorkflowType, Model model) {
 		List<Workflow> workflows = new ArrayList<>();
 		if("system".equals(displayWorkflowType)) {
-			workflows.addAll(workflowService.getWorkflowsForUser(userService.getSystemUser(), userService.getSystemUser()));
+			User systemUser = userService.getSystemUser();
+			workflows.addAll(workflowService.getWorkflowsForUser(systemUser, systemUser));
 		} else if("classes".equals(displayWorkflowType)) {
 			workflows.addAll(workflowService.getClassesWorkflows());
 		} else {
