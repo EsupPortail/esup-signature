7012aff9565aee913f909ef1765b425b26910bea
David Lemaignent <david.lemaignent@univ-rouen.fr>
2020-10-15 14:23:09 +0200

    fix empty msg

--- src/main/java/org/esupportail/esupsignature/web/controller/user/DataController.java
+++ src/main/java/org/esupportail/esupsignature/web/controller/user/DataController.java
@@ -166,7 +166,7 @@ public class DataController {
 					sendMessage = false;
 				}
 			}
-			if(sendMessage) model.addAttribute("message", new JsonMessage("help", form.getMessage()));
+			if(sendMessage && !form.getMessage().isEmpty()) model.addAttribute("message", new JsonMessage("help", form.getMessage()));
 			if (form.getDocument() != null) {
 				return "user/datas/create-pdf";
 			} else {
