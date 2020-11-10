54755f180c4f12ae3f5b8e84bb7e113c7895249e
Vincent Bonamy <vbonamy@gmail.com>
2020-11-10 18:44:22 +0100

    Use @ConditionalOnProperty for TomcatAjpConfig

--- src/main/java/org/esupportail/esupsignature/config/tomcat/TomcatAjpConfig.java
+++ src/main/java/org/esupportail/esupsignature/config/tomcat/TomcatAjpConfig.java
@@ -2,6 +2,7 @@ package org.esupportail.esupsignature.config.tomcat;
 
 import org.apache.catalina.connector.Connector;
 import org.apache.coyote.ajp.AbstractAjpProtocol;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(TomcatAjpProperties.class)
+@ConditionalOnProperty(prefix = "tomcat.ajp", name = "port")
 public class TomcatAjpConfig {
 
     private TomcatAjpProperties tomcatAjpProperties;
@@ -19,17 +21,15 @@ public class TomcatAjpConfig {
 
     @Bean
     public TomcatServletWebServerFactory servletContainer() {
-
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        if (tomcatAjpProperties.getPort() != null) {
-            Connector ajpConnector = new Connector("AJP/1.3");
-            ajpConnector.setPort(tomcatAjpProperties.getPort());
-            ajpConnector.setSecure(false);
-            ajpConnector.setAllowTrace(false);
-            ajpConnector.setScheme("http");
-            ((AbstractAjpProtocol) ajpConnector.getProtocolHandler()).setSecretRequired(false);
-            tomcat.addAdditionalTomcatConnectors(ajpConnector);
-        }
+        Connector ajpConnector = new Connector("AJP/1.3");
+        ajpConnector.setPort(tomcatAjpProperties.getPort());
+        ajpConnector.setSecure(false);
+        ajpConnector.setAllowTrace(false);
+        ajpConnector.setScheme("http");
+        ((AbstractAjpProtocol) ajpConnector.getProtocolHandler()).setSecretRequired(false);
+        tomcat.addAdditionalTomcatConnectors(ajpConnector);
         return tomcat;
     }
+
 }
