430a52add2cd299b490b153bd3bf9286393b6f11
David Lemaignent <david.lemaignent@univ-rouen.fr>
2020-11-12 10:46:41 +0100

    auto detect ajp secure conf

--- src/main/java/org/esupportail/esupsignature/config/tomcat/TomcatAjpConfig.java
+++ src/main/java/org/esupportail/esupsignature/config/tomcat/TomcatAjpConfig.java
@@ -3,31 +3,40 @@ package org.esupportail.esupsignature.config.tomcat;
 import org.apache.catalina.connector.Connector;
 import org.apache.coyote.ajp.AbstractAjpProtocol;
 import org.apache.coyote.http11.Http11NioProtocol;
+import org.esupportail.esupsignature.config.GlobalProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @Configuration
-@EnableConfigurationProperties(TomcatAjpProperties.class)
+@EnableConfigurationProperties({TomcatAjpProperties.class, GlobalProperties.class})
 @ConditionalOnProperty(prefix = "tomcat.ajp", name = "port")
 public class TomcatAjpConfig {
 
-    private TomcatAjpProperties tomcatAjpProperties;
+    private final TomcatAjpProperties tomcatAjpProperties;
 
-    public TomcatAjpConfig(TomcatAjpProperties tomcatAjpProperties) {
+    private final GlobalProperties globalProperties;
+
+    public TomcatAjpConfig(TomcatAjpProperties tomcatAjpProperties, GlobalProperties globalProperties) {
         this.tomcatAjpProperties = tomcatAjpProperties;
+        this.globalProperties = globalProperties;
     }
 
     @Bean
-    public TomcatServletWebServerFactory servletContainer() {
+    public TomcatServletWebServerFactory servletContainer() throws URISyntaxException {
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
         Connector ajpConnector = new Connector("AJP/1.3");
         ajpConnector.setPort(tomcatAjpProperties.getPort());
-        ajpConnector.setSecure(true);
         ajpConnector.setAllowTrace(false);
-        ajpConnector.setScheme("https");
+        ajpConnector.setScheme(new URI(globalProperties.getRootUrl()).getScheme());
+        if("https".equals(ajpConnector.getScheme())) {
+            ajpConnector.setSecure(true);
+        }
         ajpConnector.setAsyncTimeout(1200000);
         ajpConnector.setURIEncoding("UTF-8");
         ((AbstractAjpProtocol) ajpConnector.getProtocolHandler()).setSecretRequired(false);
