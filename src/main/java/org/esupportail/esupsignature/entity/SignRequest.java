4d36caea09cd9769e3bf9814c50ce6b16fac48ae
David Lemaignent <david.lemaignent@univ-rouen.fr>
2020-12-18 13:21:46 +0100

    fix template user ==

--- src/main/java/org/esupportail/esupsignature/entity/SignRequest.java
+++ src/main/java/org/esupportail/esupsignature/entity/SignRequest.java
@@ -61,7 +61,6 @@ public class SignRequest {
 
     @ManyToOne
     @NotNull
-    @LazyCollection(LazyCollectionOption.FALSE)
     private SignBook parentSignBook;
 
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
