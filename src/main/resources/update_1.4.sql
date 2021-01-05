c4ac6e967618e95acbd1fc2acabb7566089bc3d9
David Lemaignent <david.lemaignent@univ-rouen.fr>
2021-01-05 09:30:33 +0100

    fix 1.4 sql script

--- src/main/resources/update_1.4.sql
+++ src/main/resources/update_1.4.sql
@@ -3,9 +3,9 @@ $BODY$
 DECLARE
     lw live_workflow_workflow_steps%rowtype;
 BEGIN
-    for lw in select * from live_workflow
+    for lw in select * from live_workflow_workflow_steps
         LOOP
-            insert into live_workflow_live_workflow_steps values(lw.live_workflow_id, lw.workflow_steps_id, 0);
+            insert into live_workflow_live_workflow_steps values(lw.live_workflow_id, lw.workflow_steps_id, lw.workflow_steps_order);
         END LOOP ;
 END
 $BODY$
