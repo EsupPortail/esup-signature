081bd2f00eb11da8a1cf1f50d74c12e34c97d4fd
David Lemaignent <david.lemaignent@univ-rouen.fr>
2021-01-07 22:43:43 +0100

    spring session in h2 in memory dataSource

---
+++ src/main/java/org/esupportail/esupsignature/config/datasource/DataSourceConfig.java
@@ -0,0 +1,21 @@
+package org.esupportail.esupsignature.config.datasource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Primary
+    @Bean
+    @ConfigurationProperties(prefix="db.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+}
