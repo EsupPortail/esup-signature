25b26fffc7c7620f10f6a55cb5c4be90bd9e15a6
David Lemaignent <david.lemaignent@univ-rouen.fr>
2021-01-08 10:30:08 +0100

    spring session own transaction manager

--- src/main/java/org/esupportail/esupsignature/config/springsession/SpringSessionConfig.java
+++ src/main/java/org/esupportail/esupsignature/config/springsession/SpringSessionConfig.java
@@ -2,11 +2,13 @@ package org.esupportail.esupsignature.config.springsession;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.session.SaveMode;
 import org.springframework.session.jdbc.config.annotation.SpringSessionDataSource;
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -21,6 +23,11 @@ public class SpringSessionConfig {
                 .setType(EmbeddedDatabaseType.H2)
                 .setName("sessions")
                 .addScript("org/springframework/session/jdbc/schema-h2.sql").build();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(sessionDataSource());
     }
 
 }
