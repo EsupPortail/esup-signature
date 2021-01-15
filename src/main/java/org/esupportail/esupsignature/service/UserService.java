b7251eaf2e48f4c3193d9f0aa6522a864615f5ed
David Lemaignent <david.lemaignent@univ-rouen.fr>
2021-01-15 09:19:15 +0100

    re-enable user update

--- src/main/java/org/esupportail/esupsignature/service/UserService.java
+++ src/main/java/org/esupportail/esupsignature/service/UserService.java
@@ -22,6 +22,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -219,24 +221,24 @@ public class UserService {
         user.setEppn(eppn);
         user.setEmail(email);
         user.setUserType(userType);
-//        if(!user.getUserType().equals(UserType.system)) {
-//            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//            if (auth != null) {
-//                String userName = buildEppn(auth.getName());
-//                if (webSecurityProperties.getGroupToRoleFilterPattern() != null && eppn.equals(userName)) {
-//                    logger.info("Mise à jour des rôles de l'utilisateur " + eppn);
-//                    Collection<GrantedAuthority> authorities = (Collection<GrantedAuthority>) auth.getAuthorities();
-//                    if (authorities.size() > 0) {
-//                        user.getRoles().clear();
-//                        for (GrantedAuthority authority : authorities) {
-//                            if(authority.getAuthority().startsWith("ROLE_")) {
-//                                user.getRoles().add(authority.getAuthority());
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
+        if(!user.getUserType().equals(UserType.system)) {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            if (auth != null) {
+                String userName = buildEppn(auth.getName());
+                if (webSecurityProperties.getGroupToRoleFilterPattern() != null && eppn.equals(userName)) {
+                    logger.info("Mise à jour des rôles de l'utilisateur " + eppn);
+                    Collection<GrantedAuthority> authorities = (Collection<GrantedAuthority>) auth.getAuthorities();
+                    if (authorities.size() > 0) {
+                        user.getRoles().clear();
+                        for (GrantedAuthority authority : authorities) {
+                            if(authority.getAuthority().startsWith("ROLE_")) {
+                                user.getRoles().add(authority.getAuthority());
+                            }
+                        }
+                    }
+                }
+            }
+        }
         userRepository.save(user);
         return user;
     }
