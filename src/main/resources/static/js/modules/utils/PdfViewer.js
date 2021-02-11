6a78a43c28c06ea4d83fcee9d0f90ed837b2c268
David Lemaignent <david.lemaignent@univ-rouen.fr>
2021-02-11 10:50:10 +0100

    fix #7 : Champ multiligne dans un formulaire ne fonctionne plus

--- src/main/resources/static/js/modules/utils/PdfViewer.js
+++ src/main/resources/static/js/modules/utils/PdfViewer.js
@@ -237,24 +237,27 @@ export class PdfViewer extends EventFactory {
         console.log("save fields " + items.length);
         for (let i = 0; i < items.length; i++) {
             if(items[i].fieldName != null) {
-                let inputField = $('input[name=\'' + items[i].fieldName.split(/\$|#|!/)[0] + '\']');
-                if (inputField.val() != null) {
-                    if (inputField.is(':checkbox')) {
-                        if (!inputField[0].checked) {
-                            this.savedFields.set(items[i].fieldName, 'off');
-                        } else {
-                            this.savedFields.set(items[i].fieldName, 'on');
+                let inputName = items[i].fieldName.split(/\$|#|!/)[0];
+                let inputField = $('#' + inputName);
+                if(inputField.length > 0) {
+                    if (inputField.val() != null) {
+                        if (inputField.is(':checkbox')) {
+                            if (!inputField[0].checked) {
+                                this.savedFields.set(items[i].fieldName, 'off');
+                            } else {
+                                this.savedFields.set(items[i].fieldName, 'on');
+                            }
+                            continue;
                         }
-                        continue;
-                    }
-                    if (inputField.is(':radio')) {
-                        let radio = $('input[name=\'' + items[i].fieldName.split(/\$|#|!/)[0] + '\'][value=\'' + items[i].buttonValue + '\']');
-                        if (radio.prop("checked")) {
-                            this.savedFields.set(items[i].fieldName, radio.val());
+                        if (inputField.is(':radio')) {
+                            let radio = $('input[name=\'' + inputName + '\'][value=\'' + items[i].buttonValue + '\']');
+                            if (radio.prop("checked")) {
+                                this.savedFields.set(items[i].fieldName, radio.val());
+                            }
+                            continue;
                         }
-                        continue;
+                        this.savedFields.set(items[i].fieldName, inputField.val());
                     }
-                    this.savedFields.set(items[i].fieldName, inputField.val());
                 }
             }
         }
