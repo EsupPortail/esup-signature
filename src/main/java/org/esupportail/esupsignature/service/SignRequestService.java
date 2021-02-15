39d761bc7d5dfd7997f108f1453e4cf1ae1d2487
David Lemaignent <david.lemaignent@univ-rouen.fr>
2021-02-15 10:46:22 +0100

    Update SignRequestService.java

--- src/main/java/org/esupportail/esupsignature/service/SignRequestService.java
+++ src/main/java/org/esupportail/esupsignature/service/SignRequestService.java
@@ -327,7 +327,7 @@ public class SignRequestService {
 					for(Map.Entry<String, String> entry : formDataMap.entrySet()) {
 						List<Field> formfields = fields.stream().filter(f -> f.getName().equals(entry.getKey())).collect(Collectors.toList());
 						if(formfields.size() > 0) {
-+                                                       if(formfields.get(0).getExtValueType()!= null && !formfields.get(0).getExtValueType().equals("system")) {
+                                                       if(formfields.get(0).getExtValueType()!= null && !formfields.get(0).getExtValueType().equals("system")) {
 								List<String> steps = Arrays.asList(formfields.get(0).getStepNumbers().split(" "));
 								if (!data.getDatas().containsKey(entry.getKey()) || steps.contains(signRequest.getParentSignBook().getLiveWorkflow().getCurrentStepNumber().toString())) {
 									data.getDatas().put(entry.getKey(), entry.getValue());
