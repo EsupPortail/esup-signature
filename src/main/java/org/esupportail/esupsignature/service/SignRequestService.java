397340f890c70ff5c23eb162630a24e6769a2c10
David Lemaignent <david.lemaignent@univ-rouen.fr>
2021-05-11 11:47:15 +0200

    fix read only fields

--- src/main/java/org/esupportail/esupsignature/service/SignRequestService.java
+++ src/main/java/org/esupportail/esupsignature/service/SignRequestService.java
@@ -1110,7 +1110,7 @@ public class SignRequestService {
 		if(data != null) {
 			if(data.getForm() != null) {
 				List<Field> fields = data.getForm().getFields();
-				if (!"".equals(data.getForm().getPreFillType()) && signRequest.getParentSignBook().getLiveWorkflow().getCurrentStep().getRecipients().stream().map(Recipient::getUser).anyMatch(user1 -> user1.equals(user))) {
+				if (!"".equals(data.getForm().getPreFillType()) && signRequest.getParentSignBook().getLiveWorkflow().getCurrentStep().getUsers().contains(user)) {
 					prefilledFields = preFillService.getPreFilledFieldsByServiceName(data.getForm().getPreFillType(), fields, user, signRequest);
 					for (Field field : prefilledFields) {
 						if (signRequest.getParentSignBook().getLiveWorkflow().getCurrentStep() == null
