be9c76b9c945e95e088964911035394bb1ea083e
David Lemaignent <david.lemaignent@univ-rouen.fr>
2021-06-15 17:05:35 +0200

    view all

--- src/main/java/org/esupportail/esupsignature/service/SignRequestService.java
+++ src/main/java/org/esupportail/esupsignature/service/SignRequestService.java
@@ -242,9 +242,12 @@ public class SignRequestService {
 					break;
 			}
 		} else {
-			signRequests.addAll(signRequestRepository.findByCreateByEppn(userEppn));
 			signRequests.addAll(getToSignRequests(userEppn));
-			signRequests.removeAll(signRequestRepository.findByCreateByEppnAndStatus(userEppn, SignRequestStatus.deleted));
+			signRequests.addAll(getSignRequestsSignedByUser(userEppn));
+			signRequests.addAll(getSignRequestsRefusedByUser(userEppn));
+			signRequests.addAll(signBookService.getSignRequestByViewer(userEppn));
+			signRequests.addAll(getSharedSignedSignRequests(userEppn));
+			signRequests.addAll(signRequestRepository.findByCreateByEppnAndStatus(userEppn, SignRequestStatus.deleted));
 		}
 		return new ArrayList<>(signRequests);
 	}
