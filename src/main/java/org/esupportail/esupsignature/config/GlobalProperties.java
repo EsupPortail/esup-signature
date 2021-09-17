515d696071b16de34e6d233b722c90cc0a51b7a0
David Lemaignent <david.lemaignent@univ-rouen.fr>
2021-09-17 11:26:44 +0200

    add conf tips

--- src/main/java/org/esupportail/esupsignature/config/GlobalProperties.java
+++ src/main/java/org/esupportail/esupsignature/config/GlobalProperties.java
@@ -14,27 +14,81 @@ public class GlobalProperties implements Cloneable {
      * Chemin d'acces à l'application
      */
     private String rootUrl;
+    /**
+     * Nom de domainde ex : univ-ville.fr
+     */
     private String domain;
-    private String nexuUrl;
+    /**
+     * Chemin d'écoute de NexU
+     */
+    private String nexuUrl = "http://localhost:9795";
+    /**
+     * Version nécessaire de NexU
+     */
     private String nexuVersion;
+    /**
+     * Chemin de télechargement de NexU
+     */
     private String nexuDownloadUrl;
+    /**
+     * Masquer la tuile Créer une demande personnalisée
+     */
     private Boolean hideWizard;
+    /**
+     * Masquer la tuile Auto-signature
+     */
     private Boolean hideAutoSign;
+    /**
+     * Masquer la tuile Demander une signature
+     */
     private Boolean hideSendSignRequest;
+    /**
+     * Liste des roles faisant exception à la valeur de hideWizard
+     */
     private List<String> hideWizardExceptRoles = new ArrayList<>();
+    /**
+     * Liste des roles faisant exception à la valeur de hideAutoSign
+     */
     private List<String> hideAutoSignExceptRoles = new ArrayList<>();
+    /**
+     * Liste des roles faisant exception à la valeur de hideSendSignRequest
+     */
     private List<String> hideSendSignExceptRoles = new ArrayList<>();
+    /**
+     * Les documents des demandes terminées seront arvhivées vers ce dossier
+     */
     private String archiveUri;
+    /**
+     * Délai en nombre de jours avant que les documents ne soient effacé de la base (ne concerne que les documents à l'état archivé)
+     */
     private Integer delayBeforeCleaning = -1;
-    private Integer trashKeepDelay = -1;
-    private Boolean enableSu = false;
-    private Boolean enableSplash = false;
-    private String version = "";
-    private String applicationEmail = "esup.signature@univ-ville.fr";
-    private int hoursBeforeRefreshNotif = 24;
-    private Boolean infiniteScrolling = true;
     /**
      * Délai de conservation dans la corbeille (en jours)
+     */
+    private Integer trashKeepDelay = -1;
+    /**
+     * Activer la fonction Switch User pour les administrateurs
+     */
+    private Boolean enableSu = false;
+    /**
+     * Activer le message d'accueil pour les nouveaux utilisateurs
+     */
+    private Boolean enableSplash = false;
+    /**
+     * Géré automatiquement, ne pas modifier!
+     */
+    private String version = "";
+    private String applicationEmail = "esup.signature@univ-ville.fr";
+    /**
+     * Nombre d'heure minimum entre deux relances manuelles
+     */
+    private int hoursBeforeRefreshNotif = 24;
+    /**
+     * Activer le scrolling infini sur le tableau de bord (sinon pagination)
+     */
+    private Boolean infiniteScrolling = true;
+    /**
+     * Redirection après signature. true : retour à l'acceuil, false : on reste sur la demande
      */
     private Boolean returnToHomeAfterSign = true;
 
@@ -57,6 +111,9 @@ public class GlobalProperties implements Cloneable {
      *</ul>
      */
     private String namingTemplate = "[title]";
+    /**
+     * Suffix ajouté aux documents signés
+     */
     private String signedSuffix = "_signed";
     /**
      * Choisir le fonctionnement des délégations :
