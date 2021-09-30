39b81ffca95c75502598d33db8b1b5c4f3ec5416
David Lemaignent <david.lemaignent@univ-rouen.fr>
2021-09-30 10:45:21 +0200

    sign title

--- src/main/resources/static/js/modules/utils/PdfViewer.js
+++ src/main/resources/static/js/modules/utils/PdfViewer.js
@@ -634,7 +634,7 @@ export class PdfViewer extends EventFactory {
                     let top = Math.round(pdf.height() - ((item.rect[1] + (item.rect[3] - item.rect[1])) / .75 * this.scale));
                     let width = Math.round((item.rect[2] - item.rect[0]) / .75 * this.scale);
                     let height = Math.round((item.rect[3] - item.rect[1]) / .75 * this.scale);
-                    let signDiv = "<div id='signField_" + signFieldNumber + "' class='sign-field' style='position: absolute; left: " + left + "px; top: " + top + "px;width: " + width + "px; height: " + height + "px;'></div>";
+                    let signDiv = "<div title='Cliquez pour voir le detail de la signature' id='signField_" + signFieldNumber + "' class='sign-field' style='position: absolute; left: " + left + "px; top: " + top + "px;width: " + width + "px; height: " + height + "px;'></div>";
                     pdf.append(signDiv);
                     let signField = $('#signField_' + signFieldNumber);
                     signField.css("font-size", 8);
