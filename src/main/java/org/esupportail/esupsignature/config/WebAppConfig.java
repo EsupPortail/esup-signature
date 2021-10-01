a653be455b6e9c767355118fa482e8d03bd62c13
David Lemaignent <david.lemaignent@univ-rouen.fr>
2021-10-01 09:58:12 +0200

    fix secure

--- src/main/java/org/esupportail/esupsignature/config/WebAppConfig.java
+++ src/main/java/org/esupportail/esupsignature/config/WebAppConfig.java
@@ -90,7 +90,7 @@ public class WebAppConfig implements WebMvcConfigurer {
 		registrationBean.setOrder(5);
 		registrationBean.addUrlPatterns(
 				"/user/", "/user/*",
-				"/wssecure/", "/wssecure/*",
+				"/ws-secure/", "/ws-secure/*",
 				"/admin/", "/admin/*",
 				"/manager/", "/manager/*",
 				"/public/", "/public/*",
