c80bb3ca34d2d9ca5f4fb57af6128c1e8232a6f7
David Lemaignent <david.lemaignent@univ-rouen.fr>
2021-11-09 11:52:34 +0100

    add current step in export

--- src/main/java/org/esupportail/esupsignature/repository/RecipientRepository.java
+++ src/main/java/org/esupportail/esupsignature/repository/RecipientRepository.java
@@ -3,8 +3,5 @@ package org.esupportail.esupsignature.repository;
 import org.esupportail.esupsignature.entity.Recipient;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface RecipientRepository extends CrudRepository<Recipient, Long> {
-    List<Recipient> findByUserEppn(String userEppn);
 }
