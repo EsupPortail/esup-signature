994266ed823fcf564ecd8e661779d7951ccfe5c0
David Lemaignent <david.lemaignent@univ-rouen.fr>
2022-02-25 10:08:26 +0100

    clean tomcat conf

--- src/main/java/org/esupportail/esupsignature/config/tomcat/TomcatConfig.java
+++ src/main/java/org/esupportail/esupsignature/config/tomcat/TomcatConfig.java
@@ -1,24 +1,32 @@
 package org.esupportail.esupsignature.config.tomcat;
 
-import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.webresources.ExtractingRoot;
 import org.apache.coyote.ajp.AbstractAjpProtocol;
 import org.esupportail.esupsignature.config.GlobalProperties;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
 
+import javax.annotation.Resource;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 
 @Configuration
 @EnableConfigurationProperties({TomcatAjpProperties.class, GlobalProperties.class})
 public class TomcatConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(TomcatConfig.class);
+
+    @Resource
+    Environment environment;
 
     private final TomcatAjpProperties tomcatAjpProperties;
 
@@ -30,17 +38,9 @@ public class TomcatConfig {
     }
 
     @Bean
-    @Order(1)
     @ConditionalOnProperty(prefix = "tomcat.ajp", name = "port")
     public TomcatServletWebServerFactory servletContainer() throws URISyntaxException {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                context.setResources(new ExtractingRoot());
-                context.setReloadable(false);
-            }
-        };
-        tomcat.addContextCustomizers(cntxt -> cntxt.setReloadable(false));
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
         Connector ajpConnector = new Connector("AJP/1.3");
         ajpConnector.setPort(tomcatAjpProperties.getPort());
         ajpConnector.setAllowTrace(false);
@@ -58,14 +58,18 @@ public class TomcatConfig {
     }
 
     @Bean
-    @Order(2)
-    @ConditionalOnMissingBean(TomcatServletWebServerFactory.class)
-    public TomcatServletWebServerFactory tomcatFactory() {
-        return new TomcatServletWebServerFactory() {
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainerCustomizer() {
+        final boolean reloadable = List.of(environment.getActiveProfiles()).contains("dev");
+        logger.warn("SetReloadable to " + reloadable);
+        return new WebServerFactoryCustomizer<TomcatServletWebServerFactory>() {
             @Override
-            protected void postProcessContext(Context context) {
-                context.setResources(new ExtractingRoot());
-                context.setReloadable(false);
+            public void customize(TomcatServletWebServerFactory container) {
+                container.addContextCustomizers(
+                        cntxt -> {
+                            cntxt.setReloadable(false);
+                            cntxt.setResources(new ExtractingRoot());
+                        }
+                );
             }
         };
     }
