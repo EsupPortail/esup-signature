a2ccbc145248634bbc12047a283cc3817aa36ff0
David Lemaignent <david.lemaignent@univ-rouen.fr>
2022-05-02 10:45:59 +0200

    fix display replay btn + fix cmis export with attachment

--- src/main/java/org/esupportail/esupsignature/service/SignBookService.java
+++ src/main/java/org/esupportail/esupsignature/service/SignBookService.java
@@ -1589,9 +1589,12 @@ public class SignBookService {
                                 try {
                                     Document signedFile = signRequest.getLastSignedDocument();
                                     if (signRequest.getAttachments().size() > 0) {
-                                        targetUrl += "/" + signRequest.getTitle();
+                                        if(!targetUrl.endsWith("/")) {
+                                            targetUrl += "/";
+                                        }
+                                        targetUrl += signRequest.getTitle();
                                         for (Document attachment : signRequest.getAttachments()) {
-                                            documentService.exportDocument(documentIOType, targetUrl, attachment, null);
+                                            documentService.exportDocument(documentIOType, targetUrl, attachment, attachment.getFileName());
                                         }
                                     }
                                     String name = generateName(signRequest.getParentSignBook(), signRequest.getParentSignBook().getLiveWorkflow().getWorkflow(), signRequest.getCreateBy(), true);
