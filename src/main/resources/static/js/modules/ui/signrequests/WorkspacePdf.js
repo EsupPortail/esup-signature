0e91b230a8b734507c5a35896c14426399edd38e
David Lemaignent <david.lemaignent@univ-rouen.fr>
2022-06-15 16:45:02 +0200

    fix spots positions

--- src/main/resources/static/js/modules/ui/signrequests/WorkspacePdf.js
+++ src/main/resources/static/js/modules/ui/signrequests/WorkspacePdf.js
@@ -487,8 +487,9 @@ export class WorkspacePdf {
                 if (this.mode === 'comment') {
                     spotDiv.show();
                     signSpaceDiv.hide();
+                    let offset = $("#page_" + spot.pageNumber).offset().top - this.pdfViewer.initialOffset + (10 * (spot.pageNumber - 1));
                     spotDiv.css('left', ((parseInt(spot.posX) * this.pdfViewer.scale) - 18) + "px");
-                    spotDiv.css('top', ((parseInt(spot.posY) * this.pdfViewer.scale) - 48) + "px");
+                    spotDiv.css('top', ((parseInt(spot.posY) * this.pdfViewer.scale + offset) - 48) + "px");
                     spotDiv.width(spotDiv.width() * this.pdfViewer.scale);
                     spotDiv.unbind('mouseup');
                     spotDiv.on('mouseup', function (e) {
@@ -510,8 +511,9 @@ export class WorkspacePdf {
                     spotDiv.unbind('mouseup');
                     if (this.signable) {
                         signSpaceDiv.show();
+                        let offset = $("#page_" + spot.pageNumber).offset().top - this.pdfViewer.initialOffset + (10 * (spot.pageNumber - 1));
                         signSpaceDiv.css("top", Math.round(spot.posY * self.pdfViewer.scale / .75));
-                        signSpaceDiv.css("left", Math.round(spot.posX * self.pdfViewer.scale / .75));
+                        signSpaceDiv.css("left", Math.round((spot.posX * self.pdfViewer.scale + offset) / .75));
                         signSpaceDiv.css("width", Math.round(150 * self.pdfViewer.scale / .75) + "px");
                         signSpaceDiv.css("height", Math.round(75 * self.pdfViewer.scale / .75) + "px");
                         signSpaceDiv.css("font-size", 14 * self.pdfViewer.scale);
