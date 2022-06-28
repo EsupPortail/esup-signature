a86efc796b84a98f9011bc7b9f5709dff9474e23
David Lemaignent <david.lemaignent@univ-rouen.fr>
2022-06-28 17:36:21 +0200

    fix user dashboard

--- src/main/resources/static/js/modules/ui/signbooks/ListSignBooksUi.js
+++ src/main/resources/static/js/modules/ui/signbooks/ListSignBooksUi.js
@@ -43,13 +43,15 @@ export class ListSignBooksUi {
         $('#workflowFilter').on('change', e => this.buildUrlFilter());
         $('#recipientsFilter').on('change', e => this.buildUrlFilter());
         $('#docTitleFilter').on('change', e => this.buildUrlFilter());
-        //$('#creatorFilter').on('change', e => this.buildUrlFilter());
-        new SelectUser("creatorFilter", 1, null, this.csrf);
-        let self = this;
-        $('#creatorFilter').on('change', function(){
-            self.buildUrlFilter();
-        });
-
+        if(this.mode === "user") {
+            $('#creatorFilter').on('change', e => this.buildUrlFilter());
+        } else {
+            new SelectUser("creatorFilter", 1, null, this.csrf);
+            let self = this;
+            $('#creatorFilter').on('change', function () {
+                self.buildUrlFilter();
+            });
+        }
         $('#dateFilter').on('change', e => this.buildUrlFilter());
         $('#deleteMultipleButton').on("click", e => this.deleteMultiple());
         $('#menuDeleteMultipleButton').on("click", e => this.deleteMultiple());
