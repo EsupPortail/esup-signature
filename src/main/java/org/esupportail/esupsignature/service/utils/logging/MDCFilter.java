84a5fc90649ca85b7bacfbd86fcef47a564ee94a
David Lemaignent <david.lemaignent@univ-rouen.fr>
2022-10-17 09:41:27 +0200

    inject login into logs

---
+++ src/main/java/org/esupportail/esupsignature/service/utils/logging/MDCFilter.java
@@ -0,0 +1,27 @@
+package org.esupportail.esupsignature.service.utils.logging;
+
+import org.slf4j.MDC;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class MDCFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String name = SecurityContextHolder.getContext().getAuthentication().getName();
+            MDC.put("userId", name);
+            filterChain.doFilter(request, response);
+        } finally {
+            MDC.clear();
+        }
+    }
+}
