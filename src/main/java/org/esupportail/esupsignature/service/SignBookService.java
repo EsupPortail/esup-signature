11ed37ddb2cfbeadaa150af3daebc9f63ff8c4aa
David Lemaignent <david.lemaignent@univ-rouen.fr>
2023-01-24 11:16:04 +0100

    fix missing endDate on refuse

--- src/main/java/org/esupportail/esupsignature/service/SignBookService.java
+++ src/main/java/org/esupportail/esupsignature/service/SignBookService.java
@@ -292,20 +292,6 @@ public class SignBookService {
             status = SignRequestStatus.valueOf(statusFilter);
         }
         Page<SignBook> signBooks = signBookRepository.findSignBooksAllPaged(status, workflowFilter, docTitleFilter, creatorFilterUser, startDateFilter, endDateFilter, pageable);
-        for(SignBook signBook : signBooks) {
-            if(signBook.getEndDate() == null &&
-                    (signBook.getStatus().equals(SignRequestStatus.completed)
-                            || signBook.getStatus().equals(SignRequestStatus.exported)
-                            || signBook.getStatus().equals(SignRequestStatus.refused)
-                            || signBook.getStatus().equals(SignRequestStatus.signed)
-                            || signBook.getStatus().equals(SignRequestStatus.archived)
-                            || signBook.getStatus().equals(SignRequestStatus.deleted))) {
-                List<Action> actions = signBook.getSignRequests().stream().map(SignRequest::getRecipientHasSigned).map(Map::values).flatMap(Collection::stream).filter(action -> action.getDate() != null).sorted(Comparator.comparing(Action::getDate).reversed()).collect(Collectors.toList());
-                if(actions.size() > 0) {
-                    signBook.setEndDate(actions.get(0).getDate());
-                }
-            }
-        }
         return signBooks;
     }
 
@@ -1274,6 +1260,7 @@ public class SignBookService {
         if(data != null) {
             data.setStatus(SignRequestStatus.refused);
         }
+        signBook.setEndDate(new Date());
     }
 
     @Transactional
