67e6478e6c960ca1ffc15269c0700754218032de
David Lemaignent <david.lemaignent@univ-rouen.fr>
2023-02-03 15:09:59 +0100

    stop create users on search + test fix spring session duplicate error

--- src/main/java/org/esupportail/esupsignature/repository/UserRepository.java
+++ src/main/java/org/esupportail/esupsignature/repository/UserRepository.java
@@ -32,8 +32,8 @@ public interface UserRepository extends CrudRepository<User, Long>  {
     List<User> findByManagersRolesNotNull();
     @Query(value = "select count(*) from user_account as u where u.eppn = :eppn", nativeQuery = true)
     Long countByEppn(String eppn);
-    @Query(value = "select count(*) from user_account as u where u.email = :email", nativeQuery = true)
+    @Query(value = "select count(*) from user_account as u where upper(u.email) = :email", nativeQuery = true)
     Long countByEmail(String email);
-    Long countByEmailAndUserType(String email, UserType userType);
+    Long countByEmailIgnoreCaseAndUserType(String email, UserType userType);
     User findByPhone(String phone);
 }
