4412d07f75ec9a13927278288690b99e9704253c
David Lemaignent <david.lemaignent@univ-rouen.fr>
2023-03-15 14:50:08 +0100

    fix shib attribut control

--- src/main/java/org/esupportail/esupsignature/service/security/shib/ShibAuthenticationSuccessHandler.java
+++ src/main/java/org/esupportail/esupsignature/service/security/shib/ShibAuthenticationSuccessHandler.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -28,13 +29,12 @@ public class ShibAuthenticationSuccessHandler extends SavedRequestAwareAuthentic
         String email = httpServletRequest.getHeader("mail");
         String name = httpServletRequest.getHeader("sn");
 		String firstname = httpServletRequest.getHeader("givenName");
-		if(eppn == null || email == null || name == null || firstname == null) {
-			logger.error("At least one shib attribut is missing. Needed attributs are eppn, mail, sn and givenName");
-        	throw new EsupSignatureRuntimeException("At least one shib attribut is missing. Needed attributs are eppn, mail, sn and givenName");
-		} else {
+		if(StringUtils.hasText(eppn) && StringUtils.hasText(email) && StringUtils.hasText(name) && StringUtils.hasText(firstname)) {
 			name = new String(name.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
 			firstname = new String(firstname.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
 			userService.createUserWithAuthentication(eppn, name, firstname, email, authentication, UserType.shib);
+		} else {
+			throw new EsupSignatureRuntimeException("At least one shib attribut is missing. Needed attributs are eppn, mail, sn and givenName");
 		}
 		httpServletRequest.getSession().setAttribute("securityServiceName", "ShibSecurityServiceImpl");
 	}
