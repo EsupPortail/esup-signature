3d7e4e61de054c58865e60b7cafa3ab0d7c21bb1
David Lemaignent <david.lemaignent@univ-rouen.fr>
2023-03-16 13:58:36 +0100

    add ldap search debug logs

--- src/main/java/org/esupportail/esupsignature/service/ldap/LdapPersonService.java
+++ src/main/java/org/esupportail/esupsignature/service/ldap/LdapPersonService.java
@@ -3,6 +3,8 @@ package org.esupportail.esupsignature.service.ldap;
 import org.esupportail.esupsignature.config.ldap.LdapProperties;
 import org.esupportail.esupsignature.repository.ldap.PersonLdapLightRepository;
 import org.esupportail.esupsignature.repository.ldap.PersonLdapRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.ldap.query.LdapQueryBuilder;
@@ -17,6 +19,8 @@ import java.util.List;
 @EnableConfigurationProperties(LdapProperties.class)
 public class LdapPersonService {
 
+    private static final Logger logger = LoggerFactory.getLogger(LdapPersonService.class);
+
     @Resource
     private LdapProperties ldapProperties;
 
@@ -28,24 +32,30 @@ public class LdapPersonService {
 
     public List<PersonLdap> search(String searchString) {
         String formattedFilter = MessageFormat.format(ldapProperties.getUsersSearchFilter(), (Object[]) new String[] { searchString });
+        logger.debug("search on ldap with : " + formattedFilter);
         return (List<PersonLdap>) personLdapRepository.findAll(LdapQueryBuilder.query().countLimit(10).base(ldapProperties.getSearchBase()).filter(formattedFilter));
     }
 
     public List<PersonLdapLight> searchLight(String searchString) {
         String formattedFilter = MessageFormat.format(ldapProperties.getUsersSearchFilter(), (Object[]) new String[] { searchString });
+        logger.debug("search on ldap with : " + formattedFilter);
+
         return (List<PersonLdapLight>) personLdapLightRepository.findAll(LdapQueryBuilder.query().countLimit(10).base(ldapProperties.getSearchBase()).filter(formattedFilter));
     }
 
     public List<PersonLdapLight> getPersonLdapLight(String authName) {
         String formattedFilter = MessageFormat.format(ldapProperties.getUserIdSearchFilter(), (Object[]) new String[] { authName });
+        logger.debug("search on ldap with : " + formattedFilter);
         return (List<PersonLdapLight>) personLdapLightRepository.findAll(LdapQueryBuilder.query().countLimit(10).base(ldapProperties.getSearchBase()).filter(formattedFilter));
     }
 
     public List<PersonLdapLight> getPersonLdapLightByEppn(String eppn) {
+        logger.debug("search on ldap by eppn : " + eppn);
         return personLdapLightRepository.findByEduPersonPrincipalName(eppn);
     }
 
     public List<PersonLdap> getPersonLdapByEppn(String eppn) {
+        logger.debug("search on ldap by eppn : " + eppn);
         return personLdapRepository.findByEduPersonPrincipalName(eppn);
     }
 
