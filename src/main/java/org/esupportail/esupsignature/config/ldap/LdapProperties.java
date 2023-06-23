ea648be37cb228cbbdac8c46fe234dfc0c3d6727
David Lemaignent <david.lemaignent@univ-rouen.fr>
2023-06-23 11:08:48 +0200

    refacto ldap queries

--- src/main/java/org/esupportail/esupsignature/config/ldap/LdapProperties.java
+++ src/main/java/org/esupportail/esupsignature/config/ldap/LdapProperties.java
@@ -4,6 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import javax.annotation.PostConstruct;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "ldap")
@@ -12,7 +13,7 @@ public class LdapProperties {
     /**
      * Base de recherche des utilisateurs, ex : ou=people
      */
-    private String searchBase;
+    private String searchBase = "ou=people";
     /**
      * Filtre de recherche des utilisateurs
      */
@@ -20,31 +21,59 @@ public class LdapProperties {
     /**
      * Base de recherche des groupes, ex : ou=groups
      */
-    private String groupSearchBase;
+    private String groupSearchBase = "ou=groups";
     /**
      * Filtre utilisé pour rechercher les groupes d’un utilisateur, ex : member={0}
      */
-    private String groupSearchFilter;
+    private String groupSearchFilter = "member={0}";
     /**
-     * Filtre utilisé pour rechercher des groupes, ex : mail=*{0}
+     * Filtre utilisé pour rechercher des groupes, ex : cn=*{0}
      */
-    private String allGroupsSearchFilter;
+    private String allGroupsSearchFilter = "cn=*{0}";
     /**
      * Filtre utilisé pour retrouver les membres d’un groupe, ex : memberOf=cn={0},ou=groups,dc=univ-ville,dc=fr
      */
-    private String membersOfGroupSearchFilter;
+    private String membersOfGroupSearchFilter = "memberOf=cn={0},ou=groups,dc=univ-ville,dc=fr";
     /**
      * Filtre pour contrôler l’appartenance d’un utilisateur à un groupe, ex : &(uid={0})({1}))
      */
-    private String memberSearchFilter;
+    private String memberSearchFilter = "&(uid={0})({1}))";
     /**
      * Le champ dans lequel on trouve le login des utilisateurs récupéré au moment de l’authentification, ex : (uid={0})
      */
-    private String userIdSearchFilter;
+    private String userIdSearchFilter = "(uid={0})";
     /**
-     * Le champ dans lequel on trouve la partie gauche de l’EPPN (par défaut = userIdSearchFilter)
+     * Le champ dans lequel on trouve l'eppn des utilisateurs c'est ce champ qui sera utilisé comme identifiant unique en base, ex : (eduPersonPrincipalName={0})
+     */
+    private String userEppnSearchFilter = "(eduPersonPrincipalName={0})";
+    /**
+     * Le champ dans lequel on trouve l'email des utilisateurs , ex : (mail={0})
+     */
+    private String userMailSearchFilter = "(mail={0})";
+    /**
+     * Le champ dans lequel on trouve la partie gauche de l’EPPN (par défaut idem userIdSearchFilter)
      */
     private String eppnLeftPartSearchFilter;
+    /**
+     * Requete pour trouver les OU
+     */
+    private String ouSearchFilter = "(supannCodeEntite={0})";
+    /**
+     * Object classes correspondant aux utilisateurs (un "ou" est appliqué aux valeurs de cette liste)
+     */
+    private List<String> userObjectClasses = List.of("inetOrgPerson");
+    /**
+     * Object classes correspondant aux groupes (un "ou" est appliqué aux valeurs de cette liste)
+     */
+    private List<String> groupObjectClasses = List.of("groupOfNames");
+    /**
+     * Object classes correspondant aux OU (un "ou" est appliqué aux valeurs de cette liste)
+     */
+    private List<String> ouObjectClasses = List.of("organizationalUnit");
+    /**
+     * Object classes correspondant aux alias (un "ou" est appliqué aux valeurs de cette liste)
+     */
+    private List<String> aliasObjectClasses = List.of("nisMailAlias");
 
     @PostConstruct
     private void initEppnLeftPartSearchFilter() {
@@ -119,6 +148,22 @@ public class LdapProperties {
         this.userIdSearchFilter = userIdSearchFilter;
     }
 
+    public String getUserEppnSearchFilter() {
+        return userEppnSearchFilter;
+    }
+
+    public void setUserEppnSearchFilter(String userEppnSearchFilter) {
+        this.userEppnSearchFilter = userEppnSearchFilter;
+    }
+
+    public String getUserMailSearchFilter() {
+        return userMailSearchFilter;
+    }
+
+    public void setUserMailSearchFilter(String userMailSearchFilter) {
+        this.userMailSearchFilter = userMailSearchFilter;
+    }
+
     public Map<String, String> getMappingFiltersGroups() {
         return mappingFiltersGroups;
     }
@@ -133,5 +178,45 @@ public class LdapProperties {
 
     public void setEppnLeftPartSearchFilter(String eppnLeftPartSearchFilter) {
         this.eppnLeftPartSearchFilter = eppnLeftPartSearchFilter;
+    }
+
+    public String getOuSearchFilter() {
+        return ouSearchFilter;
+    }
+
+    public void setOuSearchFilter(String ouSearchFilter) {
+        this.ouSearchFilter = ouSearchFilter;
+    }
+
+    public List<String> getUserObjectClasses() {
+        return userObjectClasses;
+    }
+
+    public void setUserObjectClasses(List<String> userObjectClasses) {
+        this.userObjectClasses = userObjectClasses;
+    }
+
+    public List<String> getGroupObjectClasses() {
+        return groupObjectClasses;
+    }
+
+    public void setGroupObjectClasses(List<String> groupObjectClasses) {
+        this.groupObjectClasses = groupObjectClasses;
+    }
+
+    public List<String> getOuObjectClasses() {
+        return ouObjectClasses;
+    }
+
+    public void setOuObjectClasses(List<String> ouObjectClasses) {
+        this.ouObjectClasses = ouObjectClasses;
+    }
+
+    public List<String> getAliasObjectClasses() {
+        return aliasObjectClasses;
+    }
+
+    public void setAliasObjectClasses(List<String> aliasObjectClasses) {
+        this.aliasObjectClasses = aliasObjectClasses;
     }
 }
--- src/main/java/org/esupportail/esupsignature/service/ldap/LdapOrganizationalUnitService.java
+++ src/main/java/org/esupportail/esupsignature/service/ldap/LdapOrganizationalUnitService.java
@@ -1,13 +1,19 @@
 package org.esupportail.esupsignature.service.ldap;
 
 import org.esupportail.esupsignature.config.ldap.LdapProperties;
-import org.esupportail.esupsignature.repository.ldap.OrganizationalUnitLdapRepository;
 import org.esupportail.esupsignature.service.ldap.entry.OrganizationalUnitLdap;
+import org.esupportail.esupsignature.service.ldap.mapper.OrganizationalUnitLdapAttributesMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.query.LdapQuery;
+import org.springframework.ldap.query.LdapQueryBuilder;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.text.MessageFormat;
 import java.util.List;
 
 @Service
@@ -15,15 +21,28 @@ import java.util.List;
 @EnableConfigurationProperties(LdapProperties.class)
 public class LdapOrganizationalUnitService {
 
+    private static final Logger logger = LoggerFactory.getLogger(LdapOrganizationalUnitService.class);
+
     @Resource
-    private OrganizationalUnitLdapRepository organizationalUnitLdapRepository;
+    private LdapProperties ldapProperties;
+
+    @Resource
+    private LdapTemplate ldapTemplate;
 
     public List<OrganizationalUnitLdap> getOrganizationalUnitLdaps(String supannCodeEntite) {
-        return organizationalUnitLdapRepository.findBySupannCodeEntite(supannCodeEntite);
+        String formattedFilter = MessageFormat.format(ldapProperties.getOuSearchFilter(), (Object[]) new String[] { supannCodeEntite });
+        StringBuilder objectClasses = new StringBuilder();
+        for(String objectClass : ldapProperties.getOuObjectClasses()) {
+            objectClasses.append("(objectClass=").append(objectClass).append(")");
+        }
+        formattedFilter = "(&(|" + objectClasses + ")" + formattedFilter + ")";
+        logger.debug("search OrganizationalUnit by mail : " + formattedFilter);
+        LdapQuery ldapQuery = LdapQueryBuilder.query().countLimit(10).filter(formattedFilter);
+        return ldapTemplate.search(ldapQuery, new OrganizationalUnitLdapAttributesMapper());
     }
 
     public OrganizationalUnitLdap getOrganizationalUnitLdap(String supannCodeEntite) {
-        List<OrganizationalUnitLdap> organizationalUnitLdap = organizationalUnitLdapRepository.findBySupannCodeEntite(supannCodeEntite);
+        List<OrganizationalUnitLdap> organizationalUnitLdap = getOrganizationalUnitLdaps(supannCodeEntite);
         if(organizationalUnitLdap.size() > 0) {
             return organizationalUnitLdap.get(0);
         }
