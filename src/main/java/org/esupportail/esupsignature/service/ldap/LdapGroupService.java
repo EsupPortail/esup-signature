ffe94e8f6b0d3d5bd1c6ce7f4f9c78fa4aefc518
David Lemaignent <david.lemaignent@univ-rouen.fr>
2023-07-18 17:21:51 +0200

    force get members with cn

--- src/main/java/org/esupportail/esupsignature/service/ldap/LdapGroupService.java
+++ src/main/java/org/esupportail/esupsignature/service/ldap/LdapGroupService.java
@@ -131,16 +131,23 @@ public class LdapGroupService implements GroupService {
     @Override
     public List<String> getMembers(String groupName) throws EsupSignatureRuntimeException {
         List<String> eppns = new ArrayList<>();
+        String groupCn = null;
         List<Map.Entry<String, String>> group = getAllGroupsStartWith(groupName);
+        if(group.size() == 1 ) {
+            groupCn = groupName;
+        } else if (group.size() > 0) {
+            groupCn = group.stream().map(Map.Entry::getKey).toList().get(0);
+        }
+        logger.debug("getMembers of : " + groupCn);
         if (ldapProperties.getMembersOfGroupSearchFilter() != null) {
-            String formattedFilter = MessageFormat.format(ldapProperties.getMembersOfGroupSearchFilter(), groupName);
+            String formattedFilter = MessageFormat.format(ldapProperties.getMembersOfGroupSearchFilter(), groupCn);
             eppns = ldapTemplate.search(ldapProperties.getSearchBase(), formattedFilter, (ContextMapper<String>) ctx -> {
                 DirContextAdapter searchResultContext = (DirContextAdapter) ctx;
                 return searchResultContext.getStringAttribute("mail");
             });
         }
         if(group.size() > 0 && eppns.size() == 0) {
-            throw new EsupSignatureRuntimeException("empty group " + groupName);
+            throw new EsupSignatureRuntimeException("empty group " + groupCn);
         }
         return eppns;
     }
