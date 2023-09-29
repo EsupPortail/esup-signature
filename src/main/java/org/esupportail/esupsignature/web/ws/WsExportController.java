25ac8b1831d1ca56885c7f1b21f049fbb15accd8
David Lemaignent <david.lemaignent@univ-rouen.fr>
2023-09-29 10:43:56 +0200

    add ws json return switch

--- src/main/java/org/esupportail/esupsignature/web/ws/WsExportController.java
+++ src/main/java/org/esupportail/esupsignature/web/ws/WsExportController.java
@@ -1,19 +1,21 @@
 package org.esupportail.esupsignature.web.ws;
 
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletResponse;
 import org.apache.commons.io.IOUtils;
 import org.esupportail.esupsignature.entity.Form;
 import org.esupportail.esupsignature.repository.FormRepository;
 import org.esupportail.esupsignature.service.export.DataExportService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import jakarta.annotation.Resource;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.InputStream;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -37,21 +39,21 @@ public class WsExportController {
     @GetMapping(value = "/form/{name}/datas/csv", produces="text/csv")
     public ResponseEntity<Void> getFormDatasCsv(@PathVariable String name, HttpServletResponse response) {
         List<Form> forms = formRepository.findFormByNameAndDeletedIsNullOrDeletedIsFalse(name);
-        if (forms.size() > 0) {
+        if (!forms.isEmpty()) {
             try {
                 response.setContentType("text/csv; charset=utf-8");
-                response.setHeader("Content-Disposition", "inline; filename=" + URLEncoder.encode(forms.get(0).getName(), StandardCharsets.UTF_8.toString()) + ".csv");
+                response.setHeader("Content-Disposition", "inline; filename=" + URLEncoder.encode(forms.get(0).getName(), StandardCharsets.UTF_8) + ".csv");
                 InputStream csvInputStream = dataExportService.getCsvDatasFromForms(forms.stream().map(Form::getWorkflow).collect(Collectors.toList()));
                 IOUtils.copy(csvInputStream, response.getOutputStream());
-                return new ResponseEntity<>(HttpStatus.OK);
+                return ResponseEntity.ok().build();
             } catch (Exception e) {
                 logger.error("get file error", e);
             }
         } else {
             logger.warn("form " + name + " not found");
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return ResponseEntity.notFound().build();
         }
-        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        return ResponseEntity.internalServerError().build();
     }
 
     @ResponseBody
