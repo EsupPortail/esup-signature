afdf9d1e82d18dadedb19ec23617453f31b86e2b
David Lemaignent <david.lemaignent@univ-rouen.fr>
2023-10-13 10:44:04 +0200

    dto for ws sr all

--- src/main/java/org/esupportail/esupsignature/repository/SignRequestRepository.java
+++ src/main/java/org/esupportail/esupsignature/repository/SignRequestRepository.java
@@ -1,5 +1,6 @@
 package org.esupportail.esupsignature.repository;
 
+import org.esupportail.esupsignature.dto.SignRequestWsDto;
 import org.esupportail.esupsignature.entity.Comment;
 import org.esupportail.esupsignature.entity.SignRequest;
 import org.esupportail.esupsignature.entity.enums.SignRequestStatus;
@@ -8,7 +9,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,17 +16,20 @@ import java.util.Optional;
 public interface SignRequestRepository extends CrudRepository<SignRequest, Long>, PagingAndSortingRepository<SignRequest, Long> {
 
     @Query("select count(distinct  s) from SignRequest s join s.parentSignBook.liveWorkflow.currentStep.recipients r where s.status = 'pending' and s.parentSignBook.hidedBy is empty and r.user.eppn = :recipientUserEppn")
-    Long countByRecipientUserToSign(@Param("recipientUserEppn") String recipientUserEppn);
+    Long countByRecipientUserToSign(String recipientUserEppn);
 
     @Query("select distinct s from SignRequest s join s.parentSignBook.liveWorkflow.currentStep.recipients r where s.status = 'pending' and s.parentSignBook.hidedBy is empty and r.user.eppn = :recipientUserEppn")
-    List<SignRequest> signRequestByRecipientUserToSign(@Param("recipientUserEppn") String recipientUserEppn);
+    List<SignRequest> signRequestByRecipientUserToSign(String recipientUserEppn);
 
     List<SignRequest> findByIdIn(List<Long> ids);
 
     Optional<SignRequest> findByToken(String token);
 
     @Query("select distinct s from SignRequest s join s.parentSignBook.liveWorkflow.currentStep.recipients r where s.status = 'pending' and s.parentSignBook.hidedBy is empty and r.user.eppn = :recipientUserEppn")
-    List<SignRequest> findByRecipientUserToSign(@Param("recipientUserEppn") String recipientUserEppn);
+    List<SignRequest> findByRecipientUserToSign(String recipientUserEppn);
+
+    @Query("select s.id as id, s.title as title, s.status as status, s.createDate as createDate, s.createBy.eppn as createByEppn, sb.endDate as endDate from SignRequest s join SignBook as sb on sb.id = s.parentSignBook.id")
+    List<SignRequestWsDto> findAllByForWs();
 
     List<SignRequest> findByCreateByEppnAndStatus(String createByEppn, SignRequestStatus status);
 
