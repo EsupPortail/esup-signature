087c8e135ddc91a3b5d38ead56e9b2af395a9a67
David Lemaignent <david.lemaignent@univ-rouen.fr>
2024-02-15 09:14:16 +0100

    revert ScheduledTaskService.java

--- src/main/java/org/esupportail/esupsignature/service/scheduler/ScheduledTaskService.java
+++ src/main/java/org/esupportail/esupsignature/service/scheduler/ScheduledTaskService.java
@@ -18,6 +18,7 @@ import org.esupportail.esupsignature.service.WorkflowService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -28,7 +29,7 @@ import java.util.Date;
 import java.util.List;
 
 @EnableScheduling
-//@Profile("!dev")
+@Profile("!dev")
 @Component
 @EnableConfigurationProperties(GlobalProperties.class)
 public class ScheduledTaskService {
