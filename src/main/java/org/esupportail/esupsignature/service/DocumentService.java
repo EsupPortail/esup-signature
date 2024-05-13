069831d565d4c13f041c34c5bf2561402b796422
David Lemaignent <david.lemaignent@univ-rouen.fr>
2024-05-13 08:50:12 +0200

    fix smb archive

--- src/main/java/org/esupportail/esupsignature/service/DocumentService.java
+++ src/main/java/org/esupportail/esupsignature/service/DocumentService.java
@@ -105,7 +105,7 @@ public class DocumentService {
 		try {
 			URI baseURI = new URI(path.replace(" ", "%20")).normalize();
 			URI resolvedURI = baseURI.resolve(subPath.replace(" ", "%20")).normalize();
-			return exportDocument(fsAccessFactoryService.getPathIOType(path), path + resolvedURI, signedFile, name);
+			return exportDocument(fsAccessFactoryService.getPathIOType(path), path + resolvedURI.getPath(), signedFile, name);
 		} catch (EsupSignatureRuntimeException | URISyntaxException e) {
 			logger.error(e.getMessage());
 		}
