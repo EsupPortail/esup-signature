582426c778676deb42f80472a21e3c794684a964
David Lemaignent <david.lemaignent@univ-rouen.fr>
2024-05-22 09:51:26 +0200

    use x-api-key instead of bearer

--- src/main/java/org/esupportail/esupsignature/web/ws/WsControllerAdvice.java
+++ src/main/java/org/esupportail/esupsignature/web/ws/WsControllerAdvice.java
@@ -1,17 +1,27 @@
 package org.esupportail.esupsignature.web.ws;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 @ControllerAdvice(basePackages = {"org.esupportail.esupsignature.web.ws"})
 public class WsControllerAdvice {
 
+    private static final List<String> API_KEY_HEADERS = Arrays.asList("x-api-key", "x-apikey", "xapikey");
+
     @ModelAttribute("wsAccessToken")
-    public String getWsAccessToken(@RequestHeader(value = "Authorization", required = false) String authorizationHeader) {
-        if(authorizationHeader == null) return "";
-        return authorizationHeader.replace("Bearer ", "");
+    public String getWsAccessToken(HttpServletRequest request) {
+        return getApiKey(request);
     }
 
+    public String getApiKey(HttpServletRequest request) {
+        Optional<String> headerName = Collections.list(request.getHeaderNames()).stream().filter(name -> API_KEY_HEADERS.contains(name.toLowerCase())).findFirst();
+        return headerName.map(request::getHeader).orElse(null);
+    }
 
 }
