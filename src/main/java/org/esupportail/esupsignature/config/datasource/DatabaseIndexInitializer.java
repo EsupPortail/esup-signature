9fa8a82787936702c4540e2dfed4a0247c83f268
David Lemaignent <david.lemaignent@univ-rouen.fr>
2024-05-28 10:47:26 +0200

    try on create indexes

--- src/main/java/org/esupportail/esupsignature/config/datasource/DatabaseIndexInitializer.java
+++ src/main/java/org/esupportail/esupsignature/config/datasource/DatabaseIndexInitializer.java
@@ -1,11 +1,15 @@
 package org.esupportail.esupsignature.config.datasource;
 
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DatabaseIndexInitializer {
+
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseIndexInitializer.class);
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -15,11 +19,31 @@ public class DatabaseIndexInitializer {
 
     @PostConstruct
     public void init() {
-        jdbcTemplate.execute("create unique index if not exists sign_book_team_pkey on sign_book_team (sign_book_id, team_id)");
-        jdbcTemplate.execute("create unique index if not exists live_workflow_step_recipients_recipients_id_key on live_workflow_step_recipients (recipients_id)");
-        jdbcTemplate.execute("create unique index if not exists live_workflow_current_step_id_key on live_workflow (current_step_id)");
-        jdbcTemplate.execute("create unique index if not exists data_sign_book_id_key on data (sign_book_id)");
-        jdbcTemplate.execute("create unique index if not exists sign_book_hided_by_pkey on sign_book_hided_by (hided_by_id, sign_book_id)");
+        try {
+            jdbcTemplate.execute("create unique index if not exists sign_book_team_pkey on sign_book_team (sign_book_id, team_id)");
+        } catch (Exception e) {
+            logger.warn(e.getMessage());
+        }
+        try {
+            jdbcTemplate.execute("create unique index if not exists live_workflow_step_recipients_recipients_id_key on live_workflow_step_recipients (recipients_id)");
+        } catch (Exception e) {
+            logger.warn(e.getMessage());
+        }
+        try {
+            jdbcTemplate.execute("create unique index if not exists live_workflow_current_step_id_key on live_workflow (current_step_id)");
+        } catch (Exception e) {
+            logger.warn(e.getMessage());
+        }
+        try {
+            jdbcTemplate.execute("create unique index if not exists data_sign_book_id_key on data (sign_book_id)");
+        } catch (Exception e) {
+            logger.warn(e.getMessage());
+        }
+        try {
+            jdbcTemplate.execute("create unique index if not exists sign_book_hided_by_pkey on sign_book_hided_by (hided_by_id, sign_book_id)");
+        } catch (Exception e) {
+            logger.warn(e.getMessage());
+        }
     }
 
 }
