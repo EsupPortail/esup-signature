9b3c15fdad0800c53b8b9e6c944113c496eaf396
David Lemaignent <david.lemaignent@univ-rouen.fr>
2024-09-25 14:38:03 +0200

    add X-Api-Version in ws returns

--- src/main/java/org/esupportail/esupsignature/web/ws/WsControllerAdvice.java
+++ src/main/java/org/esupportail/esupsignature/web/ws/WsControllerAdvice.java
@@ -1,8 +1,16 @@
 package org.esupportail.esupsignature.web.ws;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -10,9 +18,35 @@ import java.util.List;
 import java.util.Optional;
 
 @ControllerAdvice(basePackages = {"org.esupportail.esupsignature.web.ws"})
-public class WsControllerAdvice {
+public class WsControllerAdvice implements ResponseBodyAdvice<Object> {
 
     private static final List<String> API_KEY_HEADERS = Arrays.asList("x-api-key", "x-apikey", "xapikey");
+
+    private static final String HEADER_NAME = "X-Api-Version";
+
+    private final BuildProperties buildProperties;
+
+    public WsControllerAdvice(@Autowired(required = false) BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
+    }
+
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType,
+                                  MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  ServerHttpRequest request, ServerHttpResponse response) {
+        String version = "dev";
+        if (buildProperties != null) {
+            version = buildProperties.getVersion();
+        }
+        response.getHeaders().add(HEADER_NAME, version);
+        return body;
+    }
 
     @ModelAttribute("xApiKey")
     public String getXApiKey(HttpServletRequest request) {
