12ea5c5ea434b92291239a86dbaeb971a42d286e
David Lemaignent <david.lemaignent@univ-rouen.fr>
2025-01-20 10:47:03 +0100

    keep completed otp 30 days

--- src/main/java/org/esupportail/esupsignature/service/security/otp/OtpService.java
+++ src/main/java/org/esupportail/esupsignature/service/security/otp/OtpService.java
@@ -206,8 +206,7 @@ public class OtpService {
 
     @Transactional
     public void cleanEndedOtp(){
-        List<Otp> otps = otpRepository.findBySignBookStatus(SignRequestStatus.completed);
-        otps.addAll(otpRepository.findBySignBookStatus(SignRequestStatus.deleted));
+        List<Otp> otps = otpRepository.findBySignBookStatus(SignRequestStatus.deleted);
         otps.addAll(otpRepository.findBySignBookStatus(SignRequestStatus.refused));
         otps.addAll(otpRepository.findBySignBookStatus(SignRequestStatus.exported));
         otps.addAll(otpRepository.findBySignBookStatus(SignRequestStatus.archived));
@@ -215,6 +214,12 @@ public class OtpService {
         for(Otp otp : otps) {
             clearOTP(otp.getUrlId());
             otpRepository.delete(otp);
+        }
+        for(Otp otp : otpRepository.findBySignBookStatus(SignRequestStatus.completed)) {
+            if(otp.getSignBook().getEndDate() != null && otp.getSignBook().getEndDate().before(new Date(System.currentTimeMillis() - 30L * 24 * 60 * 60 * 1000))) {
+                clearOTP(otp.getUrlId());
+                otpRepository.delete(otp);
+            }
         }
     }
 
