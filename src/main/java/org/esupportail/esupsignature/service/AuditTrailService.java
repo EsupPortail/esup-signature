a9271aaea9aced76c0e1986944f1cccd151eefb8
David Lemaignent <david.lemaignent@univ-rouen.fr>
2025-06-19 16:14:49 +0200

    log user credentials at sign

--- src/main/java/org/esupportail/esupsignature/service/AuditTrailService.java
+++ src/main/java/org/esupportail/esupsignature/service/AuditTrailService.java
@@ -18,6 +18,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.support.RequestContext;
@@ -110,7 +112,14 @@ public class AuditTrailService {
             if(authentication.getCredentials() != null){
                 authenticationDetails.put("credential", authentication.getCredentials().toString());
             }
-            authenticationDetails.put("type", authentication.getClass().getTypeName());
+            authenticationDetails.put("type", authentication.getClass().getName());
+            authenticationDetails.put("registration", authentication.getClass().getSimpleName());
+            if(authentication instanceof OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+                authenticationDetails.put("registration", oAuth2AuthenticationToken.getAuthorizedClientRegistrationId());
+                if(oAuth2AuthenticationToken.getPrincipal() instanceof DefaultOidcUser) {
+                    authenticationDetails.put("credential", ((DefaultOidcUser) oAuth2AuthenticationToken.getPrincipal()).getIdToken().getTokenValue());
+                }
+            }
         }
         auditStep.setAuthenticationDetails(authenticationDetails);
         auditStepRepository.save(auditStep);
