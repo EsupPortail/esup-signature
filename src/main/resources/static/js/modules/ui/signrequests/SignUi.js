863976800361d6ed71ca9c64036c81ff699feb1d
David Lemaignent <david.lemaignent@univ-rouen.fr>
2025-09-26 11:31:05 +0200

    fix textpart size

--- src/main/resources/static/js/modules/ui/signrequests/SignUi.js
+++ src/main/resources/static/js/modules/ui/signrequests/SignUi.js
@@ -380,14 +380,15 @@ export class SignUi {
                 'signRequestParams' : JSON.stringify(signRequestParamses, function replacer(key, value) {
                     if (this &&
                         (key === "events"
-                        || key === "cross"
-                        || key === "defaultTools"
-                        || key === "tools"
-                        || key === "signColorPicker"
-                        || key === "textareaExtra"
-                        || key === "divExtra"
-                        || key === "border"
-                        || key === "textareaPart")) {
+                            || key === "globalProperties"
+                            || key === "cross"
+                            || key === "defaultTools"
+                            || key === "tools"
+                            || key === "signColorPicker"
+                            || key === "textareaExtra"
+                            || key === "divExtra"
+                            || key === "border"
+                            || key === "textareaPart")) {
                         return undefined;
                     }
                     return value;
