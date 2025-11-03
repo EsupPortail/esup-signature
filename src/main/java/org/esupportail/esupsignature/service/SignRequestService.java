ce2be3f87c37287274919c6bafe4fa7d0a575251
David Lemaignent <david.lemaignent@univ-rouen.fr>
2025-11-03 14:40:13 +0100

    Update SignRequestService.java

--- src/main/java/org/esupportail/esupsignature/service/SignRequestService.java
+++ src/main/java/org/esupportail/esupsignature/service/SignRequestService.java
@@ -1377,7 +1377,7 @@ public class SignRequestService {
 				&& !signRequest.getStatus().equals(SignRequestStatus.exported)) {
 			throw new EsupSignatureException("Téléchargement interdit avant la fin du circuit");
 		}
-		if (!signRequest.getStatus().equals(SignRequestStatus.exported)) {
+		if (signRequest.getArchiveStatus().equals(ArchiveStatus.none)) {
 			List<Document> documents = getToSignDocuments(signRequest.getId());
 			Document document;
 			if(!documents.isEmpty()) {
@@ -1405,7 +1405,7 @@ public class SignRequestService {
     @Transactional
 	public void getToSignFileResponseWithCode(Long signRequestId, HttpServletResponse httpServletResponse) throws IOException, EsupSignatureRuntimeException, WriterException {
 		SignRequest signRequest = getById(signRequestId);
-		if (!signRequest.getStatus().equals(SignRequestStatus.exported)) {
+		if (signRequest.getArchiveStatus().equals(ArchiveStatus.none)) {
 			List<Document> documents = getToSignDocuments(signRequest.getId());
 			Document document;
 			if(!documents.isEmpty()) {
