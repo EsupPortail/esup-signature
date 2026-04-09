f4f66ef512615ea3122fea05fbf63b5fe3550727
david.lemaignent <david.lemaignent@univ-rouen.fr>
2026-04-09 08:12:28 +0000

    Edit ScheduledTaskService.java

--- src/main/java/org/esupportail/esupsignature/service/scheduler/ScheduledTaskService.java
+++ src/main/java/org/esupportail/esupsignature/service/scheduler/ScheduledTaskService.java
@@ -238,7 +238,7 @@ public class ScheduledTaskService {
 		taskService.initDssRefresh();
 	}
 
-	@Scheduled(cron="00 02 02 * * *")
+	@Scheduled(initialDelay = 12000, fixedRate = 300000000000000000000000000000000000000000)
 	public void cleanAllSignRequestDocuments() throws IOException {
 		if(globalProperties.getDocumentsHistoryDelay() > -1) {
 			List<SignRequest> signRequests = signRequestRepository.findSignRequestsByCleanDocumentsHistoryDateIsNull();
