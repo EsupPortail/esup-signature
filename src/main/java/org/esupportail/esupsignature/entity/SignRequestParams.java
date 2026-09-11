1a3f0cb5b9604532e990e4141d8e45172507b47f
Sylvain Kieffer <sylvain.kieffer@univ-paris13.fr>
2026-09-11 15:49:27 +0200

    fix: xPos et yPos n'apparaissaient plus dans l'api `/ws/workflows/{id}/scan`.

--- src/main/java/org/esupportail/esupsignature/entity/SignRequestParams.java
+++ src/main/java/org/esupportail/esupsignature/entity/SignRequestParams.java
@@ -1,5 +1,6 @@
 package org.esupportail.esupsignature.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 
 @Entity
@@ -145,6 +146,7 @@ public class SignRequestParams {
         this.signHeight = signHeight;
     }
 
+    @JsonProperty("xPos")
     public Integer getxPos() {
         return xPos;
     }
@@ -156,6 +158,7 @@ public class SignRequestParams {
         this.xPos = xPos;
     }
 
+    @JsonProperty("yPos")
     public Integer getyPos() {
         if(yPos < 0) {
             yPos = 0;
